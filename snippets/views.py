c94fd4e6b42cacd78644677708d0a158a3a41880
JON <jonn.thinkrtc@gmail.com>
2019-01-04 18:18:15 -0500

    UserListView

--- snippets/views.py
+++ snippets/views.py
@@ -1,14 +1,22 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import generics
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 # Create your views here.
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-
 class SnippetListDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
