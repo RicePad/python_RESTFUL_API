ce9f59fb9cf96f59a75d43d9dcb986ae25afcd78
JON <jonn.thinkrtc@gmail.com>
2019-01-04 18:36:09 -0500

    addded permission to restframework

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 # Create your views here.
@@ -8,10 +8,15 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(ownner=self.request.user)
 
 class SnippetListDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
