c8c740822cb5ce745e10708cfa21d4c0ba7ba937
JON <jonn.thinkrtc@gmail.com>
2019-01-04 18:15:05 -0500

    user serializer

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from snippets.models import Snippet
 
-# Register your models here.
-admin.site.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields = ('highlighted',)
+
+
+admin.site.register(Snippet, SnippetAdmin)
